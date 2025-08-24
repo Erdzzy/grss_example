36c4c5a2e7feec6bc4e401070f75b0e990bd4e11
Erdzzy <ianerdman1313@gmail.com>
2025-08-24 08:35:20 -0500

    Added files to project

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+pattern: String,
+path: std::path::PathBuf,
+}
+
+
+
+fn main() {
+ 
+let args = Cli::parse();
+   
+   println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+}
